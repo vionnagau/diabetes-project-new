ce539e3f820c032ca03a8f7d4aa4efde2411b4ef
Vionna <vionnagau@gmail.com>
2026-04-08 00:45:20 +0800

    read from new .env to upload model to hub, .env in .gitignore

--- upload_model.py
+++ upload_model.py
@@ -1,11 +1,18 @@
+import os
+from dotenv import load_dotenv
 from huggingface_hub import HfApi
+
+# Load environment variables from .env
+load_dotenv()
+token = os.getenv("HF_TOKEN")
 
 api = HfApi()
 api.upload_file(
     path_or_fileobj="Diabetesmodel.pkl",
     path_in_repo="Diabetesmodel.pkl",
     repo_id="vionnagau/diabetes-model",
-    repo_type="model"
+    repo_type="model",
+    token=token
 )
 
 print("Model uploaded successfully!")
